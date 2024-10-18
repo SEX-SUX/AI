3c037b478a3c658eaf2506f4e927f66059306fab
SEX-SUX <sachinxpapa@gmail.com>
2024-10-18 23:05:44 +0530

    ❖ | sᴧᴄʜɪɴ ꭙ sᴧɴᴧᴛᴧɴɪ | ❖

--- info.py
+++ info.py
@@ -5,15 +5,15 @@ from os import environ
 id_pattern = re.compile(r'^.\d+$')
 
 BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
-API_ID = int(os.environ.get("API_ID", ""))
-API_HASH = os.environ.get("API_HASH", "")
+API_ID = int(os.environ.get("API_ID", "28795512"))
+API_HASH = os.environ.get("API_HASH", "c17e4eb6d994c9892b8a8b6bfea4042a")
 PICS = os.environ.get("PICS", "").split()
-ADMIN = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '').split()]
-DB_URL = os.environ.get("DB_URL", "")
-DB_NAME = os.environ.get("DB_NAME", "")
-RemoveBG_API = os.environ.get("RemoveBG_API", "")
-IBB_API = os.environ.get("IBB_API", "")
-FORCE_SUB = os.environ.get("FORCE_SUB", "")
+ADMIN = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '7373125778').split()]
+DB_URL = os.environ.get("DB_URL", "postgres://iarfggbc:Vxzh_kG7cxa1kHR5faxcd1kuA4R-UT9E@rosie.db.elephantsql.com/iarfggbc")
+DB_NAME = os.environ.get("DB_NAME", "sachin")
+RemoveBG_API = os.environ.get("RemoveBG_API", "MMLEH3kH6opgSxfx7SBvqtY5")
+IBB_API = os.environ.get("IBB_API", "6a143a9c8a4ed86c483de7c239ab5990")
+FORCE_SUB = os.environ.get("FORCE_SUB", "All_SANATANI_BOT")
 PORT = os.environ.get('PORT', '8080')          
 LOG_CHANNEL = int(environ.get('LOG_CHANNEL', ''))
 LOG_TEXT = """<i><u>👁️‍🗨️USER DETAILS</u>
