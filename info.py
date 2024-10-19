352a97013f20db96b749019f98d725464c63249f
SEX-SUX <sachinxpapa@gmail.com>
2024-10-19 08:20:52 +0530

    ❖ | sᴧᴄʜɪɴ ꭙ sᴧɴᴧᴛᴧɴɪ | ❖

--- info.py
+++ info.py
@@ -5,7 +5,7 @@ from os import environ
 id_pattern = re.compile(r'^.\d+$')
 
 BOT_TOKEN = os.environ.get("BOT_TOKEN", "7476540645:AAHjMhqy2vf6KBB-BBoZWZMRIjYoo2EPyCA")
-API_ID = int(os.environ.get("API_ID", "28795512"))
+API_ID = os.environ.get("API_ID", "28795512")
 API_HASH = os.environ.get("API_HASH", "c17e4eb6d994c9892b8a8b6bfea4042a")
 PICS = os.environ.get("PICS", "https://files.catbox.moe/jutcs7.jpg").split()
 ADMIN = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '7373125778').split()]
@@ -15,7 +15,7 @@ RemoveBG_API = os.environ.get("RemoveBG_API", "MMLEH3kH6opgSxfx7SBvqtY5")
 IBB_API = os.environ.get("IBB_API", "6a143a9c8a4ed86c483de7c239ab5990")
 FORCE_SUB = os.environ.get("FORCE_SUB", "All_SANATANI_BOT")
 PORT = os.environ.get('PORT', '8080')          
-LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1002460342691'))
+LOG_CHANNEL = environ.get('LOG_CHANNEL', '-1002460342691')
 LOG_TEXT = """<i><u>👁️‍🗨️USER DETAILS</u>
 
 ○ ID : <code>{id}</code>
